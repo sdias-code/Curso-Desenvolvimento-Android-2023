105c1f5a32314abce1292f7f2b15720697c07e12
sdias-code <silviodias.ms@gmail.com>
2023-08-31 23:21:06 -0400

    Refatorado tela de layout para exibir Lista de cursos no Spinner

--- AppListaCurso/app/src/main/java/devandroid/dias/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/dias/applistacurso/view/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
         pessoaController = new PessoaController(MainActivity.this);
         pessoaController.toString();
